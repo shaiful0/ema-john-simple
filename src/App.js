4a6c244cd79e97352eafbbd3a29b1e2cd7fcbaf3
sharifulislam <shariful0401@gmail.com>
2022-04-13 01:04:16 +0600

    file added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './component/Header/Header';
 import Inventory from './component/Inventory/Inventory';
 import Login from './component/Login/Login';
 import Orders from './component/Orders/Orders';
+import RequireAuth from './component/RequireAuth/RequireAuth';
 import Shop from './component/Shop/Shop';
 import SignUp from './component/SignUp/SignUp';
 
@@ -16,7 +17,11 @@ function App() {
           <Route path='/' element={<Shop></Shop>}></Route>
           <Route path='/shop' element={<Shop></Shop>}></Route>
           <Route path='/orders' element={<Orders></Orders>}></Route>
-          <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+          <Route path='/inventory' element={
+            <RequireAuth>
+              <Inventory></Inventory>
+            </RequireAuth>
+          }></Route>
           <Route path='/about' element={<About></About>}></Route>
           <Route path='/login' element={<Login></Login>}></Route>
           <Route path='/signup' element={<SignUp></SignUp>}></Route>
